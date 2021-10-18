0e6bf9e68c697e590991a1f8645fd5ecdc07e42c
Taha Shieenavaz <tahashieenavaz@gmail.com>
2021-10-18 14:32:11 +0330

    Used constants instead of variables for Gravity!

--- gravity.cpp
+++ gravity.cpp
@@ -3,9 +3,10 @@
 #define WIDTH 600
 #define HEIGHT 600
 #define FRAMERATE 60
+#define GRAVITY 1
 
-int main( )
-{
+
+int main() {
 	struct cordinate{
 		float x;
 		float y;
@@ -18,7 +19,6 @@ int main( )
     
     float dx = 0;
     float dy = .25;
-    float gravity = 1;
     
     while (true)
     {
@@ -26,14 +26,13 @@ int main( )
 	circle(ballPosition.x, ballPosition.y,20);
 	ballPosition.x = ballPosition.x + dx;
 	ballPosition.y = ballPosition.y + dy;
-	dy = dy + gravity;
+	dy = dy + GRAVITY;
 	if(ballPosition.y > HEIGHT - 20){
 		dy = -20;
 	}
 	
 	delay(1000/FRAMERATE);
     }
-    
     
     return 0;
 }
